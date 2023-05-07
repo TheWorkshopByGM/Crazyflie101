7c445e51364c7ebc48af6c2a97d92e6229caefc5
The Workshop <80882855+TheWorkshopByGM@users.noreply.github.com>
2023-05-07 20:20:43 +0100

    Update 6_Motor_Control.py

--- 6_Motor_Control.py
+++ 6_Motor_Control.py
@@ -1,3 +1,8 @@
+"""
+Crazyflie 101 Tutorial - 6_Motor_Control.py
+Author: Guy Maalouf
+Date: May 12, 2023
+"""
 import logging
 import sys
 import time
@@ -9,13 +14,12 @@ from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 from cflib.positioning.motion_commander import MotionCommander
 from cflib.utils import uri_helper
 
-URI = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E7E7')
+URI = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E7E7') # set the URI for the Crazyflie
 
 DEFAULT_HEIGHT = 0.5
 
-deck_attached_event = Event()
-logging.basicConfig(level=logging.ERROR)
-
+deck_attached_event = Event() # create an event to detect if the flow deck is attached
+logging.basicConfig(level=logging.ERROR) # set logging level
 
 def move_linear_simple(scf):
     with MotionCommander(scf, default_height=DEFAULT_HEIGHT) as mc:
@@ -29,54 +33,51 @@ def move_linear_simple(scf):
         mc.turn_left(180)
         time.sleep(1)
 
-
 def take_off_simple(scf):
     with MotionCommander(scf, default_height=DEFAULT_HEIGHT) as mc:
         time.sleep(3)
         mc.stop()
 
-
 def param_deck_flow(_, value_str):
     value = int(value_str)
     print(value)
     if value:
-        deck_attached_event.set()
+        deck_attached_event.set() # set the event if the flow deck is attached
         print('Deck is attached!')
     else:
         print('Deck is NOT attached!')
 
 if __name__ == '__main__':
-    cflib.crtp.init_drivers()
+    cflib.crtp.init_drivers() # initialize the Crazyflie drivers
 
-    with SyncCrazyflie(URI, cf=Crazyflie(rw_cache='./cache')) as scf:
+    with SyncCrazyflie(URI, cf=Crazyflie(rw_cache='./cache')) as scf: # connect to the Crazyflie and create a SyncCrazyflie object
 
-        scf.cf.param.add_update_callback(group='deck', name='bcFlow2',
-                                         cb=param_deck_flow)
+        scf.cf.param.add_update_callback(group='deck', name='bcFlow2', cb=param_deck_flow) # add a parameter update callback for the flow deck
         time.sleep(1)
 
-        if not deck_attached_event.wait(timeout=5):
+        if not deck_attached_event.wait(timeout=5): # wait for the flow deck to be attached
             print('No flow deck detected!')
             sys.exit(1)
 
-        scf.cf.param.set_value('motorPowerSet.enable',1)
-        while(1):           
-            scf.cf.param.set_value('motorPowerSet.m1', 3000)
-            scf.cf.param.set_value('motorPowerSet.m2', 0)
-            scf.cf.param.set_value('motorPowerSet.m3', 0)
-            scf.cf.param.set_value('motorPowerSet.m4', 0)
+        scf.cf.param.set_value('motorPowerSet.enable',1) # enable the motor power
+        while(1): # infinite loop
+            scf.cf.param.set_value('motorPowerSet.m1', 3000) # set motor 1 power
+            scf.cf.param.set_value('motorPowerSet.m2', 0) # set motor 2 power
+            scf.cf.param.set_value('motorPowerSet.m3', 0) # set motor 3 power
+            scf.cf.param.set_value('motorPowerSet.m4', 0) # set motor 4 power
             time.sleep(0.25)
-            scf.cf.param.set_value('motorPowerSet.m1', 0)
-            scf.cf.param.set_value('motorPowerSet.m2', 3000)
-            scf.cf.param.set_value('motorPowerSet.m3', 0)
-            scf.cf.param.set_value('motorPowerSet.m4', 0)
+            scf.cf.param.set_value('motorPowerSet.m1', 0) # set motor 1 power
+            scf.cf.param.set_value('motorPowerSet.m2', 3000) # set motor 2 power
+            scf.cf.param.set_value('motorPowerSet.m3', 0) # set motor 3 power
+            scf.cf.param.set_value('motorPowerSet.m4', 0) # set motor 4 power
             time.sleep(0.25)
-            scf.cf.param.set_value('motorPowerSet.m1', 0)
-            scf.cf.param.set_value('motorPowerSet.m2', 0)
-            scf.cf.param.set_value('motorPowerSet.m3', 3000)
-            scf.cf.param.set_value('motorPowerSet.m4', 0)
+            scf.cf.param.set_value('motorPowerSet.m1', 0) # set motor 1 power
+            scf.cf.param.set_value('motorPowerSet.m2', 0) # set motor 2 power
+            scf.cf.param.set_value('motorPowerSet.m3', 3000) # set motor 3 power
+            scf.cf.param.set_value('motorPowerSet.m4', 0) # set motor 4 power
             time.sleep(0.25)
-            scf.cf.param.set_value('motorPowerSet.m1', 0)
-            scf.cf.param.set_value('motorPowerSet.m2', 0)
-            scf.cf.param.set_value('motorPowerSet.m3', 0)
-            scf.cf.param.set_value('motorPowerSet.m4', 3000)
+            scf.cf.param.set_value('motorPowerSet.m1', 0) # set motor 1 power
+            scf.cf.param.set_value('motorPowerSet.m2', 0) # set motor 2 power
+            scf.cf.param.set_value('motorPowerSet.m3', 0) # set motor 3 power
+            scf.cf.param.set_value('motorPowerSet.m4', 3000) # set motor 4 power
             time.sleep(0.25)
