3276c927e92dcd5734338ac9d1b3396b7077d9e5
The Workshop <80882855+TheWorkshopByGM@users.noreply.github.com>
2023-05-07 19:43:51 +0100

    Update 0_simple_connect.py

--- 0_simple_connect.py
+++ 0_simple_connect.py
@@ -1,6 +1,12 @@
+"""
+Crazyflie 101 Tutorial - 0_simple_connect.py
+Author: Guy Maalouf
+Date: May 12, 2023
+"""
+
+# Import required packages
 import logging
 import time
-
 import cflib.crtp
 from cflib.crazyflie import Crazyflie
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
@@ -9,7 +15,9 @@ from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 uri = 'radio://0/80/2M/E7E7E7E7E7'
 
 def simple_connect():
-
+    """
+    Simple function to test connection to the Crazyflie
+    """
     print("Yeah, I'm connected! :D")
     time.sleep(3)
     print("Now I will disconnect!")
@@ -17,7 +25,11 @@ def simple_connect():
 if __name__ == '__main__':
     # Initialize the low-level drivers
     cflib.crtp.init_drivers()
+
+    # Print Hello world! to console
     print("Hello world!")
+
+    # Connect to the Crazyflie and run simple_connect() function
     with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
         print("I'm in!")
         simple_connect()
